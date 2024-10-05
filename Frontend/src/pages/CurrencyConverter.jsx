3daf2a260d9bc7ec5ea0fffdef9ccb14f59fe28c
Tuddeepana <tauddeepana@gmail.com>
2024-10-05 21:01:13 +0530

    add the heading

---
+++ Frontend/src/pages/CurrencyConverter.jsx
@@ -0,0 +1,14 @@
+// Main Currency Convertor Page
+
+
+export default function CurrencyConverter() {
+  return (
+    <div>
+        {/* heading */}
+        <div className="mt-4">
+        <h1 className="text-center font-extrabold text-4xl text-green-700">Currency Converter</h1>
+        </div>
+        
+    </div>
+  )
+}
